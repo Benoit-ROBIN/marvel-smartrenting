8a949c7d44b9842110d7dec89c7c54edec1f9c64
Benoit ROBIN <benoit@MacBook-Pro-de-Benoit.local>
2019-01-31 19:13:35 +0100

    file name issue

--- front-marvel/src/App.js
+++ front-marvel/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { configureStore } from './createStore'
 
-import "./app.css";
+import "./App.css";
 import IndexPage from "./pages/index";
 import HeaderBarComponent from "./components/header";
 
